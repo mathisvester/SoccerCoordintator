903d6c1cb7e98e1718207087cd147ee24c1c7371
Mathis Vester <mathis.vester@crossconcept.de>
2017-03-03 14:11:36 +0100

    Adding data model for players

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -2,6 +2,118 @@
 
 import UIKit
 
-var str = "Hello, playground"
+// Model for Players
 
-// Mathis Kommentar
+var players = [
+  [
+    "name": "Joe Smith",
+    "height": 42,
+    "soccerExperience": true,
+    "guardianNames":  "Jim And Jan Smith"
+  ],
+  [
+    "name": "Jill Tanner",
+    "height": 36,
+    "soccerExperience": true,
+    "guardianNames":  "Clara Tanner"
+  ],
+  [
+    "name": "Bill Bon",
+    "height": 43,
+    "soccerExperience": true,
+    "guardianNames":  "Sara and Jenny Bon"
+  ],
+  [
+    "name": "Eva Gordon",
+    "height": 45,
+    "soccerExperience": false,
+    "guardianNames":  "Wendy and Mike Gordon"
+  ],
+  [
+    "name": "Matt Gill",
+    "height": 40,
+    "soccerExperience": false,
+    "guardianNames":  "Charles and Sylvia Gill"
+  ],
+  [
+    "name": "Kimmy Stein",
+    "height": 41,
+    "soccerExperience": false,
+    "guardianNames":  "Bill and Hillary Stein"
+  ],
+  [
+    "name": "Sammy Adams",
+    "height": 45,
+    "soccerExperience": false,
+    "guardianNames":  "Jeff Adams"
+  ],
+  [
+    "name": "Karl Saygan",
+    "height": 42,
+    "soccerExperience": true,
+    "guardianNames":  "Heather Bledsoe"
+  ],
+  [
+    "name": "Suzane Greenberg",
+    "height": 44,
+    "soccerExperience": true,
+    "guardianNames":  "Henrietta Dumas"
+  ],
+  [
+    "name": "Sal Dali",
+    "height": 41,
+    "soccerExperience": false,
+    "guardianNames":  "Gala Dali"
+  ],
+  [
+    "name": "Joe Kavalier",
+    "height": 39,
+    "soccerExperience": false,
+    "guardianNames":  "Sam and Elaine Kavalier"
+  ],
+  [
+    "name": "Ben Finkelstein",
+    "height": 44,
+    "soccerExperience": false,
+    "guardianNames":  "Aaron and Jill Finkelstein"
+  ],
+  [
+    "name": "Diego Soto",
+    "height": 41,
+    "soccerExperience": true,
+    "guardianNames":  "Robin and Sarika Soto"
+  ],
+  [
+    "name": "Chloe Alaska",
+    "height": 47,
+    "soccerExperience": false,
+    "guardianNames":  "David and Jamie Alaska"
+  ],
+  [
+    "name": "Arnold Willis",
+    "height": 43,
+    "soccerExperience": false,
+    "guardianNames":  "Claire Willis"
+  ],
+  [
+    "name": "Phillip Helm",
+    "height": 44,
+    "soccerExperience": true,
+    "guardianNames":  "Thomas Helm and Eva Jones"
+  ],
+  [
+    "name": "Les Clay",
+    "height": 42,
+    "soccerExperience": true,
+    "guardianNames":  "Wynonna Brown"
+  ],
+  [
+    "name": "Herschel Krustofski",
+    "height": 45,
+    "soccerExperience": true,
+    "guardianNames":  "Hyman and Rachel Krustofski"
+  ]
+]
+
+
+
